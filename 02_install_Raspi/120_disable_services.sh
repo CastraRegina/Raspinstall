fe42a91f705ddaa693bd173cfd5f3139fd80a985
Regina Castra <castraregina@gmx.net>
2023-08-06 18:36:53 +0200

    do not disable (mask) packagekit

--- 02_install_Raspi/120_disable_services.sh
+++ 02_install_Raspi/120_disable_services.sh
@@ -72,9 +72,9 @@ sudo systemctl disable apt-daily-upgrade.timer
 
 echo
 echo "disabling packagekit ..."
-sudo systemctl stop packagekit.service
-sudo systemctl disable packagekit.service
-sudo systemctl mask packagekit.service
+# sudo systemctl stop packagekit.service
+# sudo systemctl disable packagekit.service
+# sudo systemctl mask packagekit.service
 sudo systemctl stop packagekit-offline-update.service
 sudo systemctl disable packagekit-offline-update.service
 sudo systemctl mask packgekit-offline-update.service
